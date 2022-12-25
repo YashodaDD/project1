2a8a8abd876c9e0e55456e6f2a95317e4a637d09
YASHODA DAYANANDA <yashodadayananda7@gmail.com>
2022-12-25 12:23:18 +0530

    secondChange

---
+++ src/ASSIGNMENT.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import {SafeAreaView, StyleSheet, Text, StatusBar} from 'react-native';
+
+const Assignment = () => {
+  React.useEffect(() => {
+    functionOne();
+  }, []);
+
+  const functionOne = () => {
+    // const name = 'Apple';
+    // // If CONDITIONS
+    // if (name === 'BlueBerry') {
+    //   console.log('Fruit 1', name);
+    // } else if (name === 'Orange') {
+    //   console.log('Fruit 2', name);
+    // } else {
+    //   console.log('Fruit 3', 'Not Available');
+    // }
+
+    //Ternary Operator
+    // name === 'Orange'
+    //   ? console.log('Fruit', true)
+    //   : console.log('Fruit', false);
+
+    // Switch Case
+    // switch (name) {
+    //   case 'Apple':
+    //     console.log('Fruit', true);
+    //     break;
+
+    //   default:
+    //     console.log('Fruit', false);
+    //     break;
+    // }
+
+    const marks = 75;
+     75 -100 -> A
+     65 - 75 -> B
+     55 - 65 ->C
+     35 - 55 -> S
+35 - 0  => W
+
+const marks = 60;
+if (marks => 75) {
+  console.log('A');
+} else if (marks => 65) {
+  console.log('b');
+} else if (marks => 55) {
+  console.log('S');
+} else (marks => 35) {
+  console.log('w');
+}
+      };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>Lesson 01</Text>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  title: {
+    fontSize: 20,
+  },
+});
+
+export default Assignment;
